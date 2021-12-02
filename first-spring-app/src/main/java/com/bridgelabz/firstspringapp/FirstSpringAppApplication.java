01d3c383697a892050472d923cbaed7b8bae1d15
AlishaBalamwar <alishabalamwar7806@gmail.com>
2021-12-02 19:03:46 +0530

    Added: usage of logging in code

--- first-spring-app/src/main/java/com/bridgelabz/firstspringapp/FirstSpringAppApplication.java
+++ first-spring-app/src/main/java/com/bridgelabz/firstspringapp/FirstSpringAppApplication.java
@@ -1,13 +1,23 @@
 package com.bridgelabz.firstspringapp;
 
+import com.bridgelabz.firstspringapp.component.DemoBean;
+import com.bridgelabz.firstspringapp.controller.HelloWebController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class FirstSpringAppApplication {
+ public static final Logger logger = LoggerFactory.getLogger(FirstSpringAppApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(FirstSpringAppApplication.class, args);
+	public static void main(String[] args)
+	{
+		logger.debug("Welcome to Spring concept demo");
+		ApplicationContext context = SpringApplication.run(FirstSpringAppApplication.class, args);
+		DemoBean demoBean = context.getBean(DemoBean.class);
+		logger.debug("Demo Bean = "+demoBean.toString());
+		System.out.println(context.getBean(HelloWebController.class));
 	}
-
 }
---
+++ first-spring-app/src/main/java/com/bridgelabz/firstspringapp/component/DemoBean.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.firstspringapp.component;
+
+import org.springframework.stereotype.Component;
+@Component
+public class DemoBean {
+    public DemoBean(){}
+}
